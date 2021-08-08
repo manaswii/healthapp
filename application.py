cff1a330a7fa7b71f35974b238381bcb128df5ac
manaswii <manaswi.dewan@gmail.com>
2021-08-08 22:49:39 +0530

    first real commit

---
+++ application.py
@@ -0,0 +1,123 @@
+import os
+from re import X
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash, check_password_hash
+
+from helpers import toLitres, calculateSleep, cmToFeet, KgToPounds
+
+app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+#custom filters
+app.jinja_env.filters["toLitres"] = toLitres
+app.jinja_env.filters["cmToFeet"] = cmToFeet
+app.jinja_env.filters["KgToPounds"] = KgToPounds
+
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+db = SQL("sqlite:///health.db")
+
+
+
+
+@app.route("/")
+def index():
+    if session.get("user_id") == None:
+        return redirect("/login")
+
+    rows = db.execute("SELECT * FROM users JOIN information ON users.id = information.user_id WHERE users.id = ?;", session["user_id"])
+    weight = rows[0]["weight"]
+    height = rows[0]["height"]
+    age = rows[0]["age"]
+
+
+    try: 
+        info = {"waterToDrink" : int(int(weight) * 2 / 3) }
+    except:
+        info = {"waterToDrink" : ""}
+    
+    try:
+        info["sleepToGet"] = calculateSleep(age)
+    except:
+        info["sleepToGet"] = ""
+    
+    return render_template("index.html", info = info)
+
+
+@app.route("/login", methods = ["POST", "GET"])
+def login():
+    if request.method == "POST":
+        user = request.form.get("username")
+        password = request.form.get("password")
+
+        usernames = db.execute("SELECT * FROM users WHERE username = ?;", user)
+
+        if len(usernames) != 1:
+            return "username does not exist"
+        
+        if check_password_hash(usernames[0]["hash"], password) == False:
+            return "incorrect password"
+        else:
+            session["user_id"] = usernames[0]["id"]
+            return redirect ("/")
+
+    return render_template("login.html")
+        
+
+
+
+@app.route("/register", methods = ["POST", "GET"])
+def register():
+    if request.method == "POST":
+        user = request.form.get("username")
+        password = request.form.get("password")
+        confirmPassword = request.form.get("confirmation")
+        
+        usernames = db.execute("SELECT username FROM users WHERE username = ?;", user)
+
+        if not user:
+            return "enter a username"
+        elif not password:
+            return "enter a password"
+        elif len(usernames) != 0:
+            return "username already exists"
+        elif password != confirmPassword:
+            return "passwords don't match"
+
+
+        hash = generate_password_hash(password)
+        db.execute("INSERT INTO users (username, hash) VALUES (?, ?);", user, hash)
+        roww = db.execute("SELECT id FROM users WHERE username = ?", user)
+        id = roww[0]["id"]
+        db.execute("INSERT INTO information (user_id) VALUES (?);", id)
+        return redirect("/")
+
+    return render_template("register.html")
+
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/login")
+
+@app.route("/accountsettings", methods = ["GET", "POST"])
+def accountSettings():
+    if session.get("user_id") == None:
+        return redirect("/login")
+    if request.method == "POST":
+        age = request.form.get("age")
+        height = request.form.get("height")
+        weight = request.form.get("weight")
+
+        db.execute("UPDATE information SET age = ?, height = ?, weight = ? WHERE user_id = ?;", age, height, weight, session["user_id"])
+        return "upadted info :3"
+
+    fields = ["age", "weight", "height"]    
+    rows = db.execute("SELECT * FROM information WHERE user_id = ?;", session["user_id"])
+    return render_template("accountSettings.html", user = rows[0], fields = fields)
---
+++ helpers.py
@@ -0,0 +1,31 @@
+
+def toLitres(value):
+
+    return f"{value / 33.814:.2f}"
+
+def calculateSleep(age):
+    if age in range (1, 2, 1):
+        return "11 to 14 hours"
+    elif age in range (3, 5, 1):
+        return "10 to 13 hours"
+    elif age in range (6, 12, 1):
+        return "9 to 12 hours"
+    elif age in range (13, 18, 1):
+        return "8 to 10 hours"
+    elif age in range (18, 64, 1):
+        return "7 to 9 hours"
+    elif age >= 65:
+        return "7 to 8 hours"
+
+def cmToFeet(height):
+    totalInches = int(height) / 2.54
+
+    feet = int(totalInches / 12)
+    inches = totalInches % 12
+
+    return f"{feet} feet {inches:.2f} inches "
+
+def KgToPounds(weight):
+    
+    return f"{weight * 2.205:.2f} pounds"
+
