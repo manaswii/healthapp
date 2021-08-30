9d2a6947ec126c617d37722fa715ca9341e83741
manaswii <manaswi.dewan@gmail.com>
2021-08-31 00:59:17 +0530

    tried changing from sqlite to POSTRES

--- application.py
+++ application.py
@@ -29,7 +29,7 @@ app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
-db = SQL("sqlite:///health.db")
+db = SQL(os.getenv("DATABASE_URL"))
 
 
 @app.route("/", methods = ["GET", "POST"])
