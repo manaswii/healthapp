b882894a3f8e0834e1642b01b8eae9f3e01a3df7
manaswii <manaswi.dewan@gmail.com>
2021-09-02 02:04:21 +0530

    timezone fix

--- application.py
+++ application.py
@@ -207,7 +207,7 @@ def history():
     today = db.execute(f"Select *, to_char(TRANSACTED, 'DD-MM-YYYY HH24:MI') as date from history where DATE(TRANSACTED) AT TIME ZONE '{session['time_zone_3']}' = CURRENT_DATE AT TIME ZONE '{session['time_zone_3']}' AND user_id = {session['user_id']} ORDER BY TRANSACTED DESC")
     
     #older details(not including today), grouped by date
-    older = db.execute(f"SELECT to_char(date AT TIME ZONE '{session['time_zone_3']}', 'DD-MM-YYYY') AS date, glasses, sleep, calories FROM (SELECT DATE(history.TRANSACTED) as date, SUM(glasses) AS glasses, SUM(sleep) AS sleep, SUM(calories) AS calories FROM history WHERE user_id = {session['user_id']} AND DATE(TRANSACTED) AT TIME ZONE '{session['time_zone_3']}' != CURRENT_DATE AT TIME ZONE '{session['time_zone_3']}' GROUP BY DATE(history.TRANSACTED)) as hist ORDER BY hist.date DESC")
+    older = db.execute(f"SELECT to_char(date AT TIME ZONE '{session['time_zone_3']}', 'DD-MM-YYYY') AS date, glasses, sleep, calories FROM (SELECT DATE(history.TRANSACTED) AT TIME ZONE '{session['user_id']}' as date, SUM(glasses) AS glasses, SUM(sleep) AS sleep, SUM(calories) AS calories FROM history WHERE user_id = {session['user_id']} AND DATE(TRANSACTED) AT TIME ZONE '{session['time_zone_3']}' != CURRENT_DATE AT TIME ZONE '{session['time_zone_3']}' GROUP BY DATE(history.TRANSACTED)) as hist ORDER BY hist.date DESC")
     return render_template("history.html", today = today, older=older)
 
  
