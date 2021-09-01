22474bab5d3c565fbfe5bdcad20e3bbf3cf95af7
manaswii <manaswi.dewan@gmail.com>
2021-09-02 01:42:20 +0530

    fixed database, also added about section

--- application.py
+++ application.py
@@ -29,8 +29,7 @@ app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
-db = SQL("***REMOVED***")
-#db = SQL(os.getenv("DATABASE_URL"))
+db = SQL(os.getenv("DATABASE_URL"))
 
 
 @app.route("/", methods = ["GET", "POST"])
