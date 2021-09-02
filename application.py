2f92fa4ab3845cd96fa8d86f6a9881d7bcc00702
manaswii <manaswi.dewan@gmail.com>
2021-09-03 03:12:23 +0530

    fixes

--- application.py
+++ application.py
@@ -217,7 +217,7 @@ def changePassword():
 @login_required
 def history():
     #get details for today only
-    today = db.execute(f"Select *, to_char(TRANSACTED, 'DD-MM-YYYY HH24:MI') as date from history where DATE(TRANSACTED) AT TIME ZONE '{session['time_zone_3']}' = CURRENT_DATE AT TIME ZONE '{session['time_zone_3']}' AND user_id = {session['user_id']} ORDER BY TRANSACTED DESC")
+    today = db.execute(f"Select *, to_char(TRANSACTED, 'DD-MM-YYYY HH24:MI') as date from history where date_trunc('day', TRANSACTED AT TIME ZONE '{session['time_zone_3']}') = date(timezone('{session['time_zone_3']}', now())) AND user_id = {session['user_id']} ORDER BY TRANSACTED DESC")
     
     #older details(not including today), grouped by date
     older = db.execute(f"SELECT to_char(date , 'DD-MM-YYYY') AS date, glasses, sleep, calories FROM (SELECT date_trunc('day', TRANSACTED AT TIME ZONE '{session['time_zone_3']}') as date, SUM(glasses) AS glasses, SUM(sleep) AS sleep, SUM(calories) AS calories FROM history WHERE user_id = {session['user_id']} AND DATE(TRANSACTED) AT TIME ZONE '{session['time_zone_3']}' != CURRENT_DATE AT TIME ZONE '{session['time_zone_3']}' GROUP BY date) as hist ORDER BY hist.date DESC")
